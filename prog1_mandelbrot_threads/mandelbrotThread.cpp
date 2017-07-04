c8fd9f81409b33c039dd64513be8fd8970a8c974
吴启凌 <wuqiling97@163.com>
2017-07-04 23:42:46 +0800

    change task schedule to get 10x speedup

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -55,17 +55,16 @@ void* workerThreadStart(void* threadArgs) {
 	WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 	WorkerArgs& arg = *args;
 
-	// TODO: Implement worker thread here.
-
 	float dx = (arg.x1 - arg.x0) / arg.width;
 	float dy = arg.dy;
 
 	for (int j = 0; j < arg.height; j++) {
 		for (int i = 0; i < arg.width; ++i) {
 			float x = arg.x0 + i * dx;
-			float y = arg.y0 + (j + arg.startRow) * dy;
+			int row = (j * arg.numThreads + arg.threadId);
+			float y = arg.y0 + row * dy;
 
-			int index = (j * arg.width + i);
+			int index = (row * arg.width + i);
 			arg.output[index] = mandel(x, y, arg.maxIterations);
 		}
 	}
@@ -94,6 +93,10 @@ void mandelbrotThread(
 		fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
 		exit(1);
 	}
+	if (height % numThreads != 0) {
+		fprintf(stderr, "Error: height mod thread num != 0\n");
+		exit(1);
+	}
 
 	std::thread workers[MAX_THREADS - 1];
 	WorkerArgs args[MAX_THREADS];
@@ -102,7 +105,6 @@ void mandelbrotThread(
 	float dy = (y1 - y0) / height;
 
 	for (int i = 0; i < numThreads; i++) {
-		// TODO: Set thread arguments here.
 		args[i].threadId = i;
 		args[i].width = width;
 		args[i].maxIterations = maxIterations;
@@ -112,13 +114,10 @@ void mandelbrotThread(
 		args[i].dy = dy;
 
 		args[i].height = dh;
-		args[i].output = output + i*dh*width;
+		args[i].output = output;
 		args[i].y0 = y0; args[i].y1 = y1;
 		args[i].startRow = i * dh;
 
-		if (i == numThreads - 1) {
-			args[i].height = height - dh * (numThreads - 1);
-		}
 		//printf("i=%d, h = %d, startrow = %d\n", i, args[i].height, args[i].startRow);
 	}
 
